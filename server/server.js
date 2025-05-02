31db0b99e8686986dc7351098afed0dca6412dbb
Sndubey <sndubey7718@gmail.com>
2025-05-02 16:37:56 +0530

    Update CORS config to support Vercel frontend

--- server/server.js
+++ server/server.js
@@ -12,11 +12,23 @@ const connectDb = require('./utils/db');
 const errorMiddleware = require('./middlewares/error-middleware');
 const port = process.env.PORT || 5000;
 
-const corsOptions = {
-    origin: 'https://mern-project-mcjw.vercel.app',
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://mern-project-mcjw.vercel.app"
+  ];
+  
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
-}
+  };
+  
 
 app.use(cors(corsOptions));  //If any req is coming from this origin and method then give the access.
 app.use(express.json());
