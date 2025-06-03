b7f5634a7e84f46e17df389059326cfe28b14aea
Shubham Dubey <103138666+Sndubey@users.noreply.github.com>
2025-06-03 17:37:52 +0530

    Update server file for cors

--- server/server.js
+++ server/server.js
@@ -12,28 +12,28 @@ const connectDb = require('./utils/db');
 const errorMiddleware = require('./middlewares/error-middleware');
 const port = process.env.PORT || 5000;
 
-// const allowedOrigins = [
-//     "http://localhost:5173",
-//     "https://mern-project-mcjw.vercel.app"
-//   ];
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://mern-project-mcjw.vercel.app"
+  ];
   
-//   const corsOptions = {
-//     origin: function (origin, callback) {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-//     credentials: true,
-//   };
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true,
+  };
   
 
-//app.use(cors(corsOptions));  //If any req is coming from this origin and method then give the access.
+app.use(cors(corsOptions));  //If any req is coming from this origin and method then give the access.
 
 // Allow all origins (temporary)
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 app.use('/api/auth', authRoute);
